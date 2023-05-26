7435d032ed087fb8ae9aa5bed4a130ff0e852f52
wet toast <124240726+superwettoast@users.noreply.github.com>
2023-05-26 13:33:57 +1200

    added more shit

--- main/server-storage/modules/IsAdmin.lua
+++ main/server-storage/modules/IsAdmin.lua
@@ -1,13 +1,35 @@
 --module script
 local isAdmin = {
+    admin-ranks = {
+        owner = [10000000000], --game creator will always have the highest rank.
+        developer = [900],
+        head-admin = [700],
+        admim = [500],
+        moderator = [300],
+        trial-mod = [200],
+    }
     admins = {
-        
+        ["123"] = {
+            name = "name",
+            level = [200],
+        }
     },
     check = function(plr)
         for _, admin in pairs(admins) do
             if plr.id === admin then
                 return admin
+            elseif plr.id === game.OwnerId then
+                return
             end
         end
+    end,
+    add = function(plr, rank)
+        local adminThing = {
+            plr.id = {
+                name = player.Name,
+                level = rank
+            }
+        }
+        table.insert(adminThing)
     end
 }
